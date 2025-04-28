d5791dd33a0d5e1276fd7ead3b5f6265b49bf6b3
Aryan-Prasad-666 <aryanprasad6aug@gmail.com>
2025-04-28 21:28:27 +0530

    add tasks works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import os
 
@@ -8,13 +8,22 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    connection = sqlite3.connect(database_dir+"/tasks.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM tasklist')
+    tasks = cursor.fetchall() 
+    connection.close()
+    return render_template("index.html", tasks=tasks)
 
 @app.route('/', methods=['POST'])
 def addtask():
     newtask = request.form['newtask']
     connection = sqlite3.connect(database_dir+"/tasks.db")
     cursor = connection.cursor()
+    cursor.execute('INSERT INTO tasklist (task) VALUES (?)', (newtask,))
+    connection.commit()
+    connection.close()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
